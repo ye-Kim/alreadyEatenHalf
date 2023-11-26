58921e8afd5114cefea6edf72d13ae7bc0ef3100
ye-Kim <badachina1@gmail.com>
2023-11-27 03:39:04 +0900

    update person-detection functions

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # alreadyEatenHalf
 OpenSourceSW LAB
+
+## Our Project
+Detect Number of People in (Realtime) Videos using HOG Descriptor and Haarcascade Classifier
+
+### directory
+- codes : codes
+- data : test data (image and video)
+- tool : haarcascades xmls
+
+
+### used source
+- opencv haarcascades xml [https://github.com/opencv/opencv/tree/master/data/haarcascades]
+- test-Image01.jpg : [가천대학교 뉴스](https://www.gachon.ac.kr/pr/1443/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGcHIlMkY0NjQlMkY5MDMzOSUyRmFydGNsVmlldy5kbyUzRnBhZ2UlM0QxJTI2c3JjaENvbHVtbiUzRCUyNnNyY2hXcmQlM0QlMjZiYnNDbFNlcSUzRCUyNmJic09wZW5XcmRTZXElM0QlMjZyZ3NCZ25kZVN0ciUzRCUyNnJnc0VuZGRlU3RyJTNEJTI2aXNWaWV3TWluZSUzRGZhbHNlJTI2cGFzc3dvcmQlM0QlMjY%3D)
+- test-Image02.jpg : [가천대학교 뉴스](https://www.gachon.ac.kr/pr/1443/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGcHIlMkY0NjQlMkY5MTE3MyUyRmFydGNsVmlldy5kbyUzRnBhZ2UlM0QxJTI2c3JjaENvbHVtbiUzRCUyNnNyY2hXcmQlM0QlMjZiYnNDbFNlcSUzRCUyNmJic09wZW5XcmRTZXElM0QlMjZyZ3NCZ25kZVN0ciUzRCUyNnJnc0VuZGRlU3RyJTNEJTI2aXNWaWV3TWluZSUzRGZhbHNlJTI2cGFzc3dvcmQlM0QlMjY%3D)
---
+++ codes/person_detection.py
@@ -0,0 +1,66 @@
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+fullbody_xml = '../tool/haarcascade_fullbody.xml'
+face_xml = '../tool/haarcascade_frontalcatface_extended.xml'
+
+def person_detection_body(img, img_show=False):
+    body_cascade = cv2.CascadeClassifier(fullbody_xml)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    body = body_cascade.detectMultiScale(gray, 1.01, 6, minSize=(30, 30))
+    
+    num_people = len(body)
+
+    for (bx, by, bw, bh) in body:
+        cv2.rectangle(img, (bx, by), (bx+bw, by+bh), (255, 0, 0), 2)
+
+    if img_show: # plot img with rectangle
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title("Result of Detection")
+        plt.xticks([]), plt.yticks([])
+        plt.show()
+
+    return num_people
+
+def person_detection_face(img, img_show=False):
+    face_cascade = cv2.CascadeClassifier(face_xml)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    face = face_cascade.detectMultiScale(gray, 1.01, 6, minSize=(30, 30))
+    
+    num_people = len(face)
+
+    for (fx, fy, fw, fh) in face:
+        cv2.rectangle(img, (fx, fy), (fx+fw, fy+fh), (0, 255, 0), 2)
+
+    if img_show: # plot img with rectangle
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title("Result of Detection")
+        plt.xticks([]), plt.yticks([])
+        plt.show()
+
+    return num_people
+
+def person_detection_hog(img, img_show=False):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    detected, _ = hog.detectMultiScale(gray)
+
+    for (x, y, w, h) in detected:
+        cv2.rectangle(img, (x, y, w, h), (0, 0, 255), 3)
+
+    num_people = len(detected)
+    
+    if img_show:
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title("HOG image")
+        plt.show()
+
+    return num_people
